7eb9c78c0025b29bef384d3c20e60dc9e245ec02
Jeordin Callister <Callisterjeordin@gmail.com>
2025-05-08 16:41:39 -0400

    add floating terminal with lazygit

--- init.lua
+++ init.lua
@@ -33,7 +33,20 @@ require('lazy').setup({
   require 'plugins.lib.copilot',
   require 'plugins.misc.line-numbers',
   require 'plugins.misc.smear-cursor',
-
+  {
+    'voldikss/vim-floaterm',
+    -- config = function()
+    --   require('floaterm').setup {
+    --     -- Other Floaterm options
+    --     -- ... more Floaterm options ...
+    --   }
+    -- end,
+    keys = {
+      -- You might want a global keybinding to toggle Floaterm as well
+      { '<leader>ft', '<cmd>FloatermToggle<CR>',                                               desc = 'Toggle Floaterm' },
+      { '<leader>ga', '<cmd>FloatermNew --height=0.95 --width=0.85 --name=LazyGit lazygit<CR>', desc = 'Open Lazygit in Floaterm' },
+    },
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
