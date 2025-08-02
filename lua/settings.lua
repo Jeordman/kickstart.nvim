90734bc7c27096819210b7e7fa8488dff1fba61e
Jeordin Callister <Callisterjeordin@gmail.com>
2025-08-02 01:34:34 -0400

    diagnostic command

--- lua/settings.lua
+++ lua/settings.lua
@@ -74,6 +74,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code [A]ction' })
+vim.keymap.set('n', '<leader>cs', vim.diagnostic.open_float, { desc = '[C]urrent diagnostic [S]how' })
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
